bae29b58d94fa0de37a6bfb848b57ab32bdeb33d
Jasontran880 <152024215+Jasontran880@users.noreply.github.com>
2025-08-29 05:53:47 -0400

    Add menu items to navbar component & add sidebar
    Add menu items to the navbar component and style them using
    Tailwind CSS.
    
    These menu items are links to Home, Expertise, Our Approach, Contact Us
    as well as the CTA button.
    
    Add sidebar state and display the sidebar. Have button to leave sidebar
    and to open it up.
    
    Include onclick attributes to be able to close sidepanel when either
    the x is clicked or menu items are clicked.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 import assets from '../assets/assets'
 
 const Navbar = ({theme, setTheme}) => {
+
+    const[sidebarOpen, setSidebarOpen] = useState(false);
+    
   return (
 
     <div className='flex justify-between items-center px-4
@@ -10,28 +14,48 @@ const Navbar = ({theme, setTheme}) => {
     dark:bg-gray-900/70'>
 
         {/* Logo */}
-        <a href="#">
+        <a onClick={() => setSidebarOpen(false)} href="#">
             <img src={theme === 'dark' ? assets.logo_dark : assets.logo} 
-            className='w-32 sm:w-40' alt="Logo" />
+            className='w-32 sm:w-40' alt="Logo" /> 
         </a>
         
      
         {/* Menu items */}
-        <div className='text-gray-700 dark:text-white sm:text-sm max-sm:w-60
-        max-sm:pl-10 max-sm:fixe top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full
-        max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20
-        flex sm:items-center gap-5 transition-all '>
+        {/* ${} is a conditional display of navbar on mobile according to 
+        the sidebarOpen state is true */}
+
+        <div className= {`text-gray-700 dark:text-white sm:text-sm 
+        ${!sidebarOpen ? 'max-sm:w-0 overflow-hidden': 'max-sm:w-60 max-sm:pl-10'} max-sm:fixed top-0 bottom-0 right-0 
+        max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary
+        max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all`}>
+        
+        <img 
+            src={assets.close_icon} 
+            alt="close" 
+            className="w-5 absolute right-4 top-4 sm:hidden"
+            onClick={() => setSidebarOpen(false)}
+        />
+
 
         {/* Note I may not want these links to lead to seperate pages, but to just 
         reference to sections within the same page */}
-        
-            <a href="#expertise" className='sm:hover:border-b'>Expertise</a>
-            <a href="#our-approach" className='sm:hover:border-b'>Our Approach</a>
-            <a href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
+            <a onClick={() => setSidebarOpen(false)} href="#expertise" className='sm:hover:border-b'>Expertise</a>
+            <a onClick={() => setSidebarOpen(false)} href="#our-approach" className='sm:hover:border-b'>Our Approach</a>
+            <a onClick={() => setSidebarOpen(false)} href="#contact-us" className='sm:hover:border-b'>Contact Us</a>
         </div>
 
+
+
         {/* Call to Action */}
-        <div>
+        <div className='flex items-center gap-2 sm:gap-4'>
+
+            <img 
+                src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon} 
+                alt="" 
+                onClick={() => setSidebarOpen(true)} 
+                className="w-8 sm:hidden" 
+            />
+            
             <a href="#contact-us" className='text-sm max-sm:hidden flex items-center gap-2
             bg-primary text-white px-6 py-2 rounded-full cursor-pointer 
             hover:scale-103 transition-all'>
